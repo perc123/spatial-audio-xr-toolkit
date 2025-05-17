61162f34ee8ca4024d251bf24e88313106dcaf30
perc123 <61948005+perc123@users.noreply.github.com>
2025-05-17 12:25:10 +0200

    adding files

---
+++ Assets/Scripts/SpeakerInfo.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SpeakerInfo
+{
+    [SerializeField] private float posX;
+    [SerializeField] private float posY;
+    [SerializeField] private float posZ;
+    
+    [SerializeField] private float speakerGain;
+
+    [SerializeField] private string oscAddressRoot = "/speakerGain";
+
+
+}
