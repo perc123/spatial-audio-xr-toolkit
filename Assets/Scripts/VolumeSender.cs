b66877e8b411cca29272e3a16bcfa877a495da11
perc123 <61948005+perc123@users.noreply.github.com>
2026-02-08 23:38:11 +0100

    commit

--- Assets/Scripts/VolumeSender.cs
+++ Assets/Scripts/VolumeSender.cs
@@ -5,11 +5,11 @@ using extOSC;
 public class VolumeSender : MonoBehaviour
 {
     public Slider speaker1Slider;
-    public Slider speaker2Slider;
-    public Slider speaker3Slider;
-    public Slider speaker4Slider;
+    //public Slider speaker2Slider;
+    //public Slider speaker3Slider;
+    //public Slider speaker4Slider;
 
-    public OSCTransmitter transmitter; // Drag in OSCTransmitter from the OSC Manager
+    public OSCTransmitter transmitter; 
 
     void Start()
     {
@@ -19,11 +19,11 @@ public class VolumeSender : MonoBehaviour
             return;
         }
 
-        // Add listeners
+      
         speaker1Slider.onValueChanged.AddListener((val) => SendVolume("/vol1", val));
-        speaker2Slider.onValueChanged.AddListener((val) => SendVolume("/vol2", val));
+        /*speaker2Slider.onValueChanged.AddListener((val) => SendVolume("/vol2", val));
         speaker3Slider.onValueChanged.AddListener((val) => SendVolume("/vol3", val));
-        speaker4Slider.onValueChanged.AddListener((val) => SendVolume("/vol4", val));
+        speaker4Slider.onValueChanged.AddListener((val) => SendVolume("/vol4", val));*/
     }
 
     void SendVolume(string address, float volume)
